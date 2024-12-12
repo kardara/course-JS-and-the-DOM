7aaf378c055a328979e02da6046af16d9a773a9d
kardara <abdoulayekardara@gmail.com>
2024-12-12 16:21:37 +0200

    last commit

--- Mental model/highlighter.js
+++ Mental model/highlighter.js
@@ -7,25 +7,23 @@ let newContent=content.filter((i)=> i!=='')
 let content1=newContent.map((i)=>i.toLowerCase())
 //creating a function to sort words based on their frequency
 function solve(arr){
-    return arr.sort((a, b) => 
-     arr.filter(x => x === b).length - arr.filter(x => x === a).length || a - b
- );
+    return arr.sort((a, b) => arr.filter(x => x === b).length - arr.filter(x => x === a).length || a - b);
 }
 //using set to remove duplicate
 let arr= new Set(solve(content1));
 //converting the Set object into an array
 //we can also use Array.from, forEach or a simple loop
 let arr1=[...arr]
-console.log(arr1.slice(0,5)) // to check the 5 most occurantes words
 // Get the text content of 'my`paragraph' with the different tags
 let content2=par.innerHTML
 // Getting the first 5 elemets
 let a=arr1.slice(0,5)
-
+//Using for loop to iterate through the 5 elements and modify them
 for(i in a){
+    // Here we want to specify to get specifique word seperating by space
     let temp=a[i]+" "
-    content2=content2.replaceAll(temp,`<b>${temp}</b>`)
-    content2=content2.replaceAll(temp[0].toUpperCase()+temp.substr(1),`<b><u>${temp[0].toUpperCase()+temp.substr(1)}</u></b>`)
+    content2=content2.replaceAll(temp,'<span style="background-color: yellow">'+temp+'</span>')
+    content2=content2.replaceAll(temp[0].toUpperCase()+temp.slice(1),'<span style="background-color: yellow"><u>'+temp[0].toUpperCase()+temp.slice(1)+'</u></span>')
 }
 
 par.innerHTML=content2
