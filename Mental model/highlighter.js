d94e955eb6d7b5ec8875a32b936eb6cac565ee3f
kardara <abdoulayekardara@gmail.com>
2024-12-12 16:02:21 +0200

    refactoring

--- Mental model/highlighter.js
+++ Mental model/highlighter.js
@@ -1,18 +1,25 @@
 const par=document.getElementById('myParagraph');
+//extracting the text content from 'myParagraph' Replaces all non-alphanumeric characters with spaces even spaces,convert them to an array and sort the array.
 let content=par.textContent.replace(/[^a-zA-Z0-9]/g, ' ').split(' ').sort()
+//remove empty strings
 let newContent=content.filter((i)=> i!=='')
+//converting each word to lower case
 let content1=newContent.map((i)=>i.toLowerCase())
-// console.log(content1)
+//creating a function to sort words based on their frequency
 function solve(arr){
     return arr.sort((a, b) => 
      arr.filter(x => x === b).length - arr.filter(x => x === a).length || a - b
  );
 }
+//using set to remove duplicate
 let arr= new Set(solve(content1));
+//converting the Set object into an array
+//we can also use Array.from, forEach or a simple loop
 let arr1=[...arr]
-console.log(arr1.slice(0,5))
-
+console.log(arr1.slice(0,5)) // to check the 5 most occurantes words
+// Get the text content of 'my`paragraph' with the different tags
 let content2=par.innerHTML
+// Getting the first 5 elemets
 let a=arr1.slice(0,5)
 
 for(i in a){
